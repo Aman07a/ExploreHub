6cccbc875340e133d57ebce4eeae6ce3d930c0d7
Aman07a <aman16.aa17@gmail.com>
2024-03-31 16:23:56 +0200

    Added CRUD to Tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -5,6 +5,25 @@ class TagsController < ApplicationController
 
   def show
     @tag = Tag.find(params[:id])
-    @destinations = @tag.destinations
+  end
+
+  def new
+    @tag = Tag.new
+  end
+
+  def create
+    @tag = Tag.new(tag_params)
+
+    if @tag.save
+      redirect_to tag_path(@tag), notice: 'Tag was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def tag_params
+    params.require(:tag).permit(:title, :image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,11 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Tags
+  resources :tags, only: [:index, :new, :create, :show]
 
-  get "/tags", to: "tags#index"
-
-  get '/tags/:id', to: 'tags#show', as: :tag
+  # Destinations
+  get "/destinations", to: "destinations#index"
+  get "/destinations/new", to: "destinations#new"
+  # post "/destinations", to: "destinations#create"
 end
