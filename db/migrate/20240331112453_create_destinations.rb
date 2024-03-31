ced82cc6861a118b848001cbef4bef69e09cf1e1
Aman07a <aman16.aa17@gmail.com>
2024-03-31 13:48:52 +0200

    Database Migrations + Seeders

--- db/migrate/20240331112453_create_destinations.rb
+++ db/migrate/20240331112453_create_destinations.rb
@@ -1,7 +1,10 @@
 class CreateDestinations < ActiveRecord::Migration[7.1]
   def change
     create_table :destinations do |t|
-
+      t.string :name
+      t.string :image
+      t.string :description
+      t.references :tag, foreign_key: true
       t.timestamps
     end
   end
