e35ca5a90460147a22e95616bda614d4c16829da
Aman07a <aman16.aa17@gmail.com>
2024-04-01 14:11:44 +0200

    Validating: if tag_id exist

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,3 +1,12 @@
 class Destination < ApplicationRecord
   belongs_to :tag
+
+  validates :tag_id, presence: true
+  validate :existing_tag
+
+  private
+
+  def existing_tag
+    errors.add(:tag_id, "must be an existing tag") unless Tag.exists?(id: tag_id)
+  end
 end
